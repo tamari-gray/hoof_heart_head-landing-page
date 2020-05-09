4d23a2635066436499fe49f2bfee65dc90dc96a1
tamari-gray <tamari-gray>
2020-05-09 20:34:07 +1000

    mobile finished

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+</script>
+
+<style>
+  /* * {
+    outline: 5px solid red;
+  } */
+  main {
+    position: relative;
+    z-index: 10;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+  .background-img {
+    height: 80vh;
+    width: 100vw;
+    position: absolute;
+    z-index: 0;
+    opacity: 0.5;
+  }
+  .gradient {
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    z-index: 10;
+    /* background: #8e2de2; fallback for old browsers */
+    /* background: -webkit-linear-gradient(
+      to right,
+      #4a00e0,
+      #8e2de2
+    ); Chrome 10-25, Safari 5.1-6 */
+    /* background: linear-gradient(
+      to right,
+      #4a00e0,
+      #8e2de2 */
+    /* ); W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+</style>
+
+<!-- <img src="grandad_love.jpg" class="img-fit-cover background-img " alt="..." />
+<div class="gradient" /> -->
+
+<Nav />
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,166 @@
+<script>
+  import * as animateScroll from "svelte-scrollto";
+  var audio = new Audio("fart_sound_scroll.mp3");
+
+  function playFart() {
+    audio.muted = false;
+    audio.play();
+  }
+
+  // function scroll() {
+  //   y = 110;
+  // }
+
+  let y;
+
+  $: if (y >= 100) {
+    console.log(`playing fart!`);
+    playFart();
+  } else {
+    audio.muted = true;
+  }
+</script>
+
+<style>
+  @font-face {
+    src: url(chela.ttf);
+    font-family: chela;
+  }
+  h1,
+  p {
+    margin: 0 auto;
+    color: black;
+    font-family: "Raleway", sans-serif;
+  }
+
+  h1 {
+    font-size: 2em;
+    margin: auto;
+  }
+  p {
+    margin-bottom: 0.3em;
+
+    font-size: 0.8em;
+    font-family: roboto;
+
+    margin-top: 1em;
+  }
+
+  .space-top {
+    height: 10vh;
+  }
+  .bottom-space {
+    height: 11vh;
+  }
+
+  .subText {
+    font-size: 1em;
+    margin-top: 0.8em;
+    font-style: italic;
+    font-family: "Roboto";
+  }
+
+  img {
+    height: 150px;
+    -ms-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+
+  @media (min-width: 380px) {
+    .warm-break {
+      display: none;
+    }
+    img {
+      height: 170px;
+    }
+    .bottom-space {
+      height: 16vh;
+    }
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 2em;
+    }
+    .space-top {
+      height: 20vh;
+    }
+    .subText,
+    p {
+      font-size: 1em;
+    }
+    .cont {
+      padding-left: 0.4rem;
+    }
+    img {
+      height: 250px;
+    }
+  }
+
+  @media (min-width: 1000px) {
+    h1 {
+      font-size: 3em;
+    }
+    img {
+      height: 300px;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<svelte:window bind:scrollY={y} />
+
+<div class="container cont">
+  <div class="columns">
+    <div class="column col-8 col-mx-auto">
+
+      <div class="space-top" />
+    </div>
+    <div class="column col-12 columns">
+      <div class="columns column col-sm-12 col-8 col-ml-auto">
+        <div class="column col-12 col-sm-10 col-mx-auto">
+          <h1>
+            Feel your families
+            <!-- <span style="color: #76ff03; font-style: italic">warm</span> -->
+            <span style="color: #76ff03">warm</span>
+            <br class="warm-break" />
+            energy, by sending them
+            <br />
+            <span style="color: #76ff03;">YOUR FARTS!</span>
+          </h1>
+          <h1 class="subText" style="">
+            Coming soon for
+            <br />
+            Iphone & Android
+          </h1>
+
+        </div>
+
+        <div class="column col-12" style="height: 2.5vh" />
+
+        <div class="column col-8 col-sm-10 col-mx-auto">
+          <p>Sign up to get early access now!</p>
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-input"
+              placeholder="Email address"
+              on:click={() => animateScroll.scrollToBottom()} />
+            <button
+              class="btn btn-primary input-group-btn"
+              style="background: #EB03FF; border-color:#A400B3 ">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="column col-12 bottom-space" style="" />
+      <div class="column col-sm-4 col-mx-auto col-3">
+        <img src="app_mockup.png" style="" alt="..." />
+      </div>
+    </div>
+  </div>
+</div>
